e39d153f79c0d0abeb895404687fac1e703668d1
Aurore <aurore.martin504@gmail.com>
2025-04-10 12:06:18 +0200

    update to landing text again still too large on phone screens

--- src/components/Pages/404/NotFoundPage.js
+++ src/components/Pages/404/NotFoundPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hamburger from '../../Reusable/Header/Hamburger';
 import ToggleButton from '../../Reusable/Header/ToggleButton';
 import './NotFoundPage.css';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
@@ -9,6 +10,9 @@ const NotFoundPage = () => {
     <ToggleButton />
     <Hamburger />
       <h2>404 Page Not Found 😐</h2>
+      <Link to="/" className="back-button">
+        ⬅ Back to Main Page
+      </Link>
     </div>
   );
 };
